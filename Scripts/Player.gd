1e0e3400d5d11fd33758dc4fdb445d03022af6e9
Artem Khrulkov <artem.khrulkov@fastdev.se>
2020-07-11 21:51:14 +0500

    Added enemy; fixes;

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -9,37 +9,37 @@ var velocity = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-	
+    pass
+    
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	velocity.y += GRAVITY
-	if Input.is_action_pressed("ui_right"):
-		velocity.x = SPEED
-		$AnimatedSprite.flip_h = false
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -SPEED
-		$AnimatedSprite.flip_h = true
-	else:
-		velocity.x = 0
-	
-	if is_on_floor():
-		if Input.is_action_just_pressed("ui_up"):
-			velocity.y = JUMP_HEIGHT
-			
-	velocity = move_and_slide(velocity, UP)
+func _physics_process(_delta):
+    velocity.y += GRAVITY
+    if Input.is_action_pressed("ui_right"):
+        velocity.x = SPEED
+        $AnimatedSprite.flip_h = false
+    elif Input.is_action_pressed("ui_left"):
+        velocity.x = -SPEED
+        $AnimatedSprite.flip_h = true
+    else:
+        velocity.x = 0
+    
+    if is_on_floor():
+        if Input.is_action_just_pressed("ui_up"):
+            velocity.y = JUMP_HEIGHT
+            
+    velocity = move_and_slide(velocity, UP)
 
-	if velocity.length() >= 0:
-		$AnimatedSprite.play()
-	else:
-		$AnimatedSprite.stop()
-	
-	if velocity.x != 0 and is_on_floor():
-		$AnimatedSprite.animation = "run"
-	elif velocity.y < 0:
-		$AnimatedSprite.animation = "jump"
-	else:
-		$AnimatedSprite.animation = "idle"
-		
-	if Input.is_action_pressed("ui_fire"):
-		$Gun.DoShot($AnimatedSprite.flip_h)
+    if velocity.length() >= 0:
+        $AnimatedSprite.play()
+    else:
+        $AnimatedSprite.stop()
+    
+    if velocity.x != 0 and is_on_floor():
+        $AnimatedSprite.animation = "run"
+    elif velocity.y < 0:
+        $AnimatedSprite.animation = "jump"
+    else:
+        $AnimatedSprite.animation = "idle"
+        
+    if Input.is_action_pressed("ui_fire"):
+        $Gun.DoShot($AnimatedSprite.flip_h)
