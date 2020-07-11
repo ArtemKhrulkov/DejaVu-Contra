4ec031e2aa5880769c9aa7212039f838826b028b
Artem Khrulkov <artem.khrulkov@fastdev.se>
2020-07-11 17:42:21 +0500

    Small fixes

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,14 +1,11 @@
 extends KinematicBody2D
 
 const SPEED = 400  # How fast the player will move (pixels/sec).
-var velocity = Vector2()
 const UP = Vector2(0, -1);
 const GRAVITY = 20
 const JUMP_HEIGHT = -500
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
+var velocity = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -40,9 +37,6 @@ func _physics_process(delta):
     
     if velocity.x != 0 and is_on_floor():
         $AnimatedSprite.animation = "run"
-        $AnimatedSprite.flip_v = false
-        # See the note below about boolean assignment
-        $AnimatedSprite.flip_h = velocity.x < 0
     elif velocity.y < 0:
         $AnimatedSprite.animation = "jump"
     else:
